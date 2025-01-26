ab916138ed07838e6202f2f9f3d2c4d40cac146c
Aniketps <aniketp2327@gmail.com>
2025-01-27 03:48:12 +0530

    first commit

--- test1..py
+++ test1..py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, requests
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -11,4 +11,4 @@ def bye():
     return jsonify({"bye" : "Bye Aniket"})
 
 if __name__ == '__main__':
-    app.run(debug=true)
+    app.run(debug=false)
