cb6844305d29d7b1d1bd2fb8c371f18824e93e56
Aniketps <aniketp2327@gmail.com>
2025-01-27 03:45:33 +0530

    first commit

--- response1.py
+++ response1.py
@@ -1,3 +1,7 @@
 import requests
 
 response = requests.get("https://aniketapi.ancientcoders.in/greet")
+if(response.status_code == 200):
+    print(response.json())
+    
+print(response.status_code)
--- test1..py
+++ test1..py
@@ -1,4 +1,4 @@
-from flask import flask, jsonify, requests
+from flask import Flask, jsonify, requests
 
 app = Flask(__name__)
 
